e7183baccbe699b728241e6de5aa5e379d02e6c9
m ihsan <mschodeihsan@gmail.com>
2021-12-04 07:09:26 +0300

    ping.go

--- misc/commands/ping.go
+++ misc/commands/ping.go
@@ -1,1 +1,22 @@
+package misc
 
+import (
+	"time"
+
+	"github.com/XzFrosT/crunch/core"
+	"github.com/XzFrosT/crunch/utils/emojis"
+)
+
+var PingCommand = &core.Command{
+	Name:        "ping",
+	Description: "Get bot latency",
+	Handler: func(ctx *core.CommandContext) {
+		latency := time.Duration(ctx.State.PacerLoop.EchoBeat.Get() - ctx.State.PacerLoop.SentBeat.Get())
+		if latency <= 0 {
+			ctx.Reply(emojis.PingPong, "There aren't enough latency measurements yet ;(")
+			return
+		}
+
+		ctx.Reply(emojis.PingPong, "Pong, %dms.", latency.Milliseconds())
+	},
+}
