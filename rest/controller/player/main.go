167b85eb9f3b227c4ed0b2bdcb5ed0bf92eda843
m ihsan <mschodeihsan@gmail.com>
2021-12-04 07:01:48 +0300

    music player

--- rest/controller/player/main.go
+++ rest/controller/player/main.go
@@ -1,1 +1,23 @@
+package player
 
+import (
+	music "github.com/XzFrosT/crunch/music/audio"
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Index(c *fiber.Ctx) error {
+	guildId, _ := discord.ParseSnowflake(c.Params("id"))
+	player := music.GetPlayer(discord.GuildID(guildId))
+
+	if !guildId.IsValid() || player == nil || player.Voicy == nil {
+		return fiber.ErrNotFound
+	}
+
+	return c.JSON(&fiber.Map{"data": &fiber.Map{
+		"current":  player.Current,
+		"queue":    player.Queue,
+		"state":    player.State,
+		"position": player.Voicy.Position,
+	}, "error": nil})
+}
