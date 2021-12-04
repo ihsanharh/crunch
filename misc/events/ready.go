88b713d633e77eedbd86873d17bfdd939dc883ae
m ihsan <mschodeihsan@gmail.com>
2021-12-04 07:12:30 +0300

    ready event

--- misc/events/ready.go
+++ misc/events/ready.go
@@ -1,1 +1,15 @@
+package misc
 
+import (
+	"os"
+
+	"github.com/XzFrosT/crunch/core"
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/diamondburned/arikawa/v3/gateway"
+)
+
+var ReadyEvent = &core.Event{
+	Handler: func(_ *gateway.ReadyEvent) {
+		core.State.UpdateStatus(gateway.UpdateStatusData{Activities: []discord.Activity{{Name: os.Getenv("DISCORD_STATUS"), Type: discord.ListeningActivity}}})
+	},
+}
